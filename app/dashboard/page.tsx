41372f86f2df860b2b1cfd29685ed652b043b8ac
Yhonny Aplicano <yhonny296@gmail.com>
2024-04-28 22:25:32 -0600

    [Add] - Funcionalidad de crear notas, editarlas y eliminarlas

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,116 @@
+import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+import Link from "next/link";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Edit, File, Trash } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { revalidatePath } from "next/cache";
+import { TrashDelete } from "@/components/SubmitButtons";
 
+// Esta funcion es para obtener todas las notas del usuario
+async function getData(userId: string) {
+  const data = await prisma.note.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
+  return data;
+}
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  // Notas del usuario
+  const data = await getData(user?.id as string);
+
+  // Esta función es para enviar la información al servidor, Se elimina la nota
+  async function deleteNote(formData: FormData) {
+    'use server'
+
+    if (!user) {
+      throw new Error('Not authorized')
+    }
+
+    // Se obtiene el id de la nota de un input que no se ve en el formulario
+    // Se obtiene asi ya que no se puede pasar directamente el id de la nota extraido de la data
+    const noteId = formData.get('noteId') as string
+
+    await prisma.note.delete({
+      where: {
+        id: noteId,
+        userId: user?.id,
+      }
+    })
+
+    // Esto es porque al borrar la nota siempre queda en cache por lo que se sigue viendo en la UI, con esto se actualiza la página
+    revalidatePath('/dashboard')
+  }
+
   return (
-    <div>DashboardPage</div>
+    <div className="grid items-start gap-y-8">
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl md:text-4xl font-bold">Yout Notes</h1>
+          <p className="text-lg text-muted-foreground">Here you can see and create new notes</p>
+        </div>
+
+        <Button asChild>
+          <Link href="/dashboard/new">
+            Create a new Note
+          </Link>
+        </Button>
+      </div>
+
+      {data.length < 1 ? (
+        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+          <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
+            <File className="w-10 h-10 text-primary" />
+          </div>
+
+          <h2 className="mt-6 text-xl font-semibold">You dont have any notes created</h2>
+          <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">You currently dont have any notes. Please create some so that you can see them right here.</p>
+
+          <Button asChild>
+            <Link href="/dashboard/new">
+              Create a new Note
+            </Link>
+          </Button>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-y-4">
+          {data.map((note) => (
+            <Card key={note.id} className="flex items-center justify-between p-4">
+              <div>
+                <h2 className="font-semibold text-xl text-primary">{note.title}</h2>
+                {/* Esta es una forma de mostrar la fecha de creación de la nota, son parametros de la función Intl.DateTimeFormat */}
+                <p>{new Intl.DateTimeFormat('en-US', {
+                  dateStyle: 'full',
+                  timeStyle: 'short'
+                }).format(new Date(note.createdAt))}</p>
+              </div>
+
+              <div className="flex gap-x-4">
+                <Link href={`/dashboard/new/${note.id}`}>
+                  <Button variant={"outline"} size={"icon"}>
+                    <Edit className="w-4 h-4" />
+                  </Button>
+                </Link>
+
+                <form action={deleteNote}>
+                  <input type="hidden" name="noteId" value={note.id} />
+                  <TrashDelete />
+                </form>
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
--- components/SubmitButtons.tsx
+++ components/SubmitButtons.tsx
@@ -1,52 +1,79 @@
-'use client'
+"use client";
 
-import { useFormStatus } from "react-dom"
-import { Button } from "./ui/button"
-import { Loader2 } from "lucide-react"
-
+import { useFormStatus } from "react-dom";
+import { Button } from "./ui/button";
+import { Loader2, Trash } from "lucide-react";
 
 export default function SubmitButtons() {
-
-    // Get the status of the form
-    // Por ejemplo, si el formulario está en estado de carga, se puede mostrar un botón de "Por favor espere"
-    const {pending} = useFormStatus()
-
-  return (
-    <>
-    {pending ? (
-        <Button disabled className="w-fit"><Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait</Button>
-    ) : (
-        <Button type="submit" className="w-fit">Save Now</Button>
-    )}
-    </>
-  )
-}
-
-
-export function StripeSubscriptionCreationButton() {
-  const {pending} = useFormStatus()
+  // Get the status of the form
+  // Por ejemplo, si el formulario está en estado de carga, se puede mostrar un botón de "Por favor espere"
+  const { pending } = useFormStatus();
 
   return (
     <>
       {pending ? (
-        <Button disabled className="w-full"><Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait</Button>
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
       ) : (
-        <Button type="submit" className="w-full">Create Subscription</Button>
+        <Button type="submit" className="w-fit">
+          Save Now
+        </Button>
       )}
     </>
-  )
+  );
+}
+
+export function StripeSubscriptionCreationButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-full">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full">
+          Create Subscription
+        </Button>
+      )}
+    </>
+  );
 }
 
 export function StripePortal() {
-  const {pending} = useFormStatus()
+  const { pending } = useFormStatus();
 
   return (
     <>
       {pending ? (
-        <Button disabled className="w-fit"><Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait</Button>
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
       ) : (
-        <Button type="submit" className="w-fit">View payment details</Button>
+        <Button type="submit" className="w-fit">
+          View payment details
+        </Button>
       )}
     </>
-  )
+  );
+}
+
+export function TrashDelete() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled variant={"destructive"} size={"icon"}>
+          <Loader2 className=" w-4 h-4 animate-spin" />
+        </Button>
+      ) : (
+        <Button variant={"destructive"} size={"icon"} type="submit">
+          <Trash className="w-4 h-4" />
+        </Button>
+      )}
+    </>
+  );
 }

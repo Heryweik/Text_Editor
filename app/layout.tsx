920cf9504fe46e641e6bb8f4da86d96e46fb107e
Yhonny Aplicano <yhonny296@gmail.com>
2024-05-01 00:07:25 -0600

    [Changes] - Favicon y Icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,24 +45,19 @@ export default async function RootLayout({
     <html lang="en">
       {/* Iconos */}
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
         <link
           rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
       </head>
 
       {/* si el usuario no esta logueado, se le asignara el colorScheme por defecto */}
